1b0ff7f09d8169c132861adb86d9538e9f47a1ce
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-24 14:30:56 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -15,7 +15,7 @@ import time
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
-    from configss import Config
+    from configs import Config
 
 # the Strings used for this "thing"
 from translation import Translation
