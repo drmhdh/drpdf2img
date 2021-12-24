618fb0efb254d096a360ea50268f5e076e022952
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-24 16:01:20 +0530

    Create localisation.py

---
+++ LOCAL/localisation.py
@@ -0,0 +1,23 @@
+START_TEXT = "Send me any file to begin."
+
+FORCE_SUB_TEXT = "In order to use this bot, you've to join my parent channel."
+
+CHANNEL_LINK = "https://t.me/DroneBots"
+
+SUPPORT_LINK = "https://t.me/TeamDrone"
+
+info_text = "This bot is developed by @ChauhanMahesh\n\nWritten in python library TELETHON.\n\nBot by : @DroneBots\nSupport : @TeamDrone\n\nV1.1"   
+
+help_text = "Send me any video file to start compressing it or any kind of file to rename it.\n\n/compress - negligible loss compression\n/convert - change formats or extract audio of any video\n/rename - rename any file, extension not required\n/trim - cut your videos" 
+
+source_text = "Deploy your own bot.\n\nMain branch - Personal use\nPublic branch - For your channel"
+
+DEV = "https://t.me/ChauhanMahesh"
+
+spam_notice = "This bot is hosted on heroku, and hence can run just run one process at a time.Spamming the bot or encoding adult videos will lead you to a ban."
+
+JPG = "LOCAL/video_convertor.jpg"
+
+JPG2 = "LOCAL/20211215_165751.jpg"
+
+JPG3 = "LOCAL/PicsArt_12-16-08.57.15.jpg"
