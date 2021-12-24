2a27ae299d6b3f730a355930ca2de695637ab493
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-24 13:47:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,10 +22,7 @@ else:
     from configs import Config
 
 import pyrogram
-# Get logging configurations
-logging.config.fileConfig('logging.conf')
-logging.getLogger().setLevel(logging.ERROR)
-logging.getLogger("pyrogram").setLevel(logging.ERROR)
+
 
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
