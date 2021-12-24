1b74ba2986e7564f6be8372b63ece4429f3d528f
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-24 11:12:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,6 +9,8 @@ logging.basicConfig(level=logging.DEBUG,
 logger = logging.getLogger(__name__)
 
 import os
+from info import SESSION, API_ID, API_HASH, BOT_TOKEN
+
 
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
@@ -20,6 +22,19 @@ import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
+class Bot(Client):
+
+    def __init__(self):
+        super().__init__(
+            session_name=SESSION,
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            workers=50,
+            plugins={"root": "plugins"},
+            sleep_threshold=5,
+        )
+
 if __name__ == "__main__" :
     # create download directory, if not exist
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
@@ -27,12 +42,12 @@ if __name__ == "__main__" :
     plugins = dict(
         root="plugins"
     )
-    app = bot(
+    """app = bot(
         "pdf2img",
         bot_token=Config.TG_BOT_TOKEN,
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
         plugins=plugins
-    )
+    )"""
     #Config.AUTH_USERS.add(1337144652)
     app.run()
