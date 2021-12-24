f9c66ba9b91081b4997ffbb6473595a3d90f18e1
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-24 20:19:22 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -29,10 +29,10 @@ def is_enabled(value, default):
     
 
 # Bot information
-SESSION = environ.get('SESSION', 'pdf2img')
+"""SESSION = environ.get('SESSION', 'pdf2img')
 API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
-API_TOKEN = environ['API_TOKEN']    
+API_TOKEN = environ['API_TOKEN']"""    
 
 
 
@@ -40,9 +40,9 @@ API_TOKEN = environ['API_TOKEN']
     
 # Config Variables
 class Config(object):
-    #API_ID = int(os.environ.get("API_ID"))
-    #API_HASH = os.environ.get("API_HASH")
-    #API_TOKEN = os.environ.get("API_TOKEN")
+    API_ID = int(os.environ.get("API_ID"))
+    API_HASH = os.environ.get("API_HASH")
+    API_TOKEN = os.environ.get("API_TOKEN")
     UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL")
     CONVERT_API = os.environ.get("CONVERT_API")
     MAX_FILE_SIZE = os.environ.get("MAX_FILE_SIZE")
